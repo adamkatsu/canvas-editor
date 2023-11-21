ec771e8b04dc5cbaaed76949c37c4cef739f00b1
Adam Katsu <adamkatsu369@gmail.com>
2023-11-21 14:34:19 +0700

    upload image function

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 var stage = new Konva.Stage({
     container: 'container',   // id of container <div>
-    width: 800,
-    height: 800,
+    width: window.innerWidth - 100,
+    height: window.innerHeight - 100,
   });
 
-  // then create layer
 var layer = new Konva.Layer();
-
-// create our shape
-var circle = new Konva.Circle({
-    x: stage.width() / 2,
-    y: stage.height() / 2,
-    radius: 100,
-    fill: 'red',
-    stroke: 'black',
-    strokeWidth: 4,
-    draggable: true
-});
-
 var background = new Konva.Rect({
     x: 0,
     y: 0,
@@ -26,8 +13,19 @@ var background = new Konva.Rect({
     fill: 'white',
     listening: false
 });
-
+// create our shape
+var circle = new Konva.Circle({
+    id: 'circle',
+    x: stage.width() / 2,
+    y: stage.height() / 2,
+    radius: 100,
+    fill: 'red',
+    stroke: 'black',
+    strokeWidth: 4,
+    draggable: true
+});
 var rect1 = new Konva.Rect({
+    id: 'rect',
     x: 20,
     y: 20,
     width: 200,
@@ -39,22 +37,55 @@ var rect1 = new Konva.Rect({
   });
   // add the shape to the layer
 
+let shapes = [circle, rect1];
+let imagesArr = [];
+let currentImg = [];
 
-let dragEvent = (x) => {
+const inputImg = document.getElementById('input-img');
+const output = document.getElementById('output');
+
+inputImg.addEventListener("change", () => {
+    const file = inputImg.files;
+    imagesArr.push(file[0]);
+    currentImg.push(file[0]);
+    displayImages();
+});
+
+function displayImages() {
+    currentImg.forEach((image, index) => {
+        Konva.Image.fromURL(`${URL.createObjectURL(image)}`, function (newImage) {
+            newImage.setAttrs({
+              x: stage.width() / 2,
+              y: stage.width() / 2,
+              width: image.naturalWidth,
+              height: image.naturalHeight,
+              draggable: true
+            });
+            layer.add(newImage);
+            dragEvent(newImage);
+            currentImg.pop();
+        });
+        // layer.add(Konva.Transformer.nodes([newImage]));
+    });
+}
+imagesArr.forEach((image) => {
+    dragEvent(image);
+});
+shapes.forEach((shape) => {
+    dragEvent(shape);
+});
+function dragEvent(x) {
     x.on('dragmove', function() {
-        x.stroke('white');
-        x.opacity(0.3);
-        x.scaleX(0.8);
-        x.scaleY(0.8);
+        x.opacity(0.8);
+        x.scaleX(0.9);
+        x.scaleY(0.9);
     });
     x.on('dragend', function() {
-        x.stroke('black');
         x.opacity(1);
         x.scaleX(1);
         x.scaleY(1);
     });
-}  
-let shapes = [circle, rect1];
+}
 
 
 
@@ -67,7 +98,6 @@ layer.add(rect1);
 
 layer.add(tr)
 tr.nodes([rect1]);
-tr.nodes([circle]);
 
 
 // add the layer to the stage
